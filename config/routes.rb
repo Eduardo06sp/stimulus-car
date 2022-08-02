673e7d5cafb30ae4db5a7df5186f3752a27a1d51
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-08-01 22:47:51 -0700

    feat: generate CarsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'cars/index'
+  get 'cars/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
