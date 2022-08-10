edf6d17090be2f732366475c6ce87f4b247d4355
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-08-10 00:59:48 -0700

    feat: append Trim fields in the trim#addNewFields method

--- app/javascript/controllers/trim_controller.js
+++ app/javascript/controllers/trim_controller.js
@@ -1,6 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
+  static targets = ['newTrimFields']
+
+  addNewFields() {
+    const trimForm = this.newTrimFieldsTarget.content.cloneNode(true);
+    this.element.append(trimForm);
   }
 }
