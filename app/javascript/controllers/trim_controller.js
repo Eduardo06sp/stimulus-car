6fa861e7541c667df16c27f2697947edf23e365a
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-08-10 22:55:41 -0700

    feat: generate unique values for Trim fields in new Car view

--- app/javascript/controllers/trim_controller.js
+++ app/javascript/controllers/trim_controller.js
@@ -5,6 +5,19 @@ export default class extends Controller {
 
   addNewFields() {
     const trimForm = this.newTrimFieldsTarget.content.cloneNode(true);
+    const trimLabels = trimForm.querySelectorAll('label');
+    const trimInputs = trimForm.querySelectorAll('input');
+    const currentTime = Date.now();
+
+    const generateUniqueValue = function(element, attribute) {
+      const currentAttributeValue = element.getAttribute(attribute);
+      return currentAttributeValue.replace('0', currentTime);
+    };
+
+    trimLabels.forEach((label) => label.setAttribute('for', generateUniqueValue(label, 'for')));
+    trimInputs.forEach((input) => input.setAttribute('id', generateUniqueValue(input, 'id')));
+    trimInputs.forEach((input) => input.setAttribute('name', generateUniqueValue(input, 'name')));
+
     this.element.append(trimForm);
   }
 }
